945ac244d4f8adaf1d0b5c05142bc32b034410b5
Leonardo Grasso <me@leonardograsso.com>
2017-07-04 00:24:46 +0200

    New: Aliases for macOS apps

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -5,7 +5,24 @@ alias ll='ls -lha'
 
 alias reload='source ~/.bash_profile'
 
+# OSX lockscreen
 alias lockscreen='/System/Library/CoreServices/"Menu Extras"/User.menu/Contents/Resources/CGSession -suspend'
 alias bye='lockscreen'
 
+# Mute/Unmute the system volume. Plays nice with all other volume settings.
+alias mute="osascript -e 'set volume output muted true'"
+alias unmute="osascript -e 'set volume output muted false'"
+
+# Apps
+alias f='open -a Finder '
+alias fh='open -a Finder .'
+alias textedit="open -a TextEdit"
+alias chrome="open -a google\ chrome"
+alias safari="open -a safari"
+alias firefox="open -a firefox"
+if [ -s /usr/bin/firefox ] ; then
+  unalias firefox
+fi
+
+# Other stuff
 alias alpine='docker run -it --rm -v ~/:/Users/$(whoami) alpine sh'
