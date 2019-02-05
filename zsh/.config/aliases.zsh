a0624866c708b004863bae9e9c45f66d18cd7a64
leogr <me@leonardograsso.com>
2019-02-05 17:15:14 +0100

    New: Initial ZSH configuration

---
+++ zsh/.config/aliases.zsh
@@ -0,0 +1,9 @@
+alias dotfiles='code ~/.dotfiles'
+alias reload='source ~/.zshrc'
+
+alias ls='ls -G'
+alias ll='ls -lha'
+
+# GNOME3 lockscreen
+alias lockscreen='xdg-screensaver lock'
+alias bye='lockscreen'
