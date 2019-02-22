534b7a746b3732fb36dad4d57e8e1cc04c77b6c0
Leonardo Grasso <me@leonardograsso.com>
2019-02-22 23:36:25 +0100

    Fix: Install script

--- install.sh
+++ install.sh
@@ -5,15 +5,17 @@ set -xeuo pipefail
 # Install or update
 if [ -e "${HOME}/.dotfiles" ]; then
     echo "~> .dotfiles folder already found in ${HOME}. Updating..."
-    git -C ~/.dotfiles pull
+    git -C $HOME/.dotfiles pull
 else
     echo "~> Installing .dotfiles folder in ${HOME}..."
-    git clone https://github.com/leogr/dotfiles ~/.dotfiles
+    git clone https://github.com/leogr/dotfiles $HOME/.dotfiles
 fi
 
 # Stow packages
 echo "~> Stow packages..."
+pushd $HOME/.dotfiles
 bash stow.sh
+popd
 
 # Install git-code
 bash <(curl -fsSL https://leogr.github.io/git-code/install.sh)
