555edc7298045f378f98c4cd00719830d7ce3102
Leonardo Grasso <me@leonardograsso.com>
2021-03-23 17:55:41 +0100

    new(zsh): add zsh-autosuggestions
    Signed-off-by: Leonardo Grasso <me@leonardograsso.com>

--- install.sh
+++ install.sh
@@ -6,10 +6,17 @@ set -xeuo pipefail
 if [ -e "${HOME}/.dotfiles" ]; then
     echo "~> .dotfiles folder already found in ${HOME}. Updating..."
     git -C $HOME/.dotfiles pull
+
+    git -C $HOME/.dotfiles/zsh/.config/zsh/plugins/zsh-autosuggestions
 else
     echo "~> Installing .dotfiles folder in ${HOME}..."
     git clone https://github.com/leogr/dotfiles $HOME/.dotfiles
+    
+    # Install tools
+    git clone https://github.com/zsh-users/zsh-autosuggestions \
+        ${HOME}/.dotfiles/zsh/.config/zsh/plugins/zsh-autosuggestions
 fi
+
 
 # Stow packages
 echo "~> Stow packages..."
@@ -17,5 +24,5 @@ pushd $HOME/.dotfiles
 bash stow.sh
 popd
 
-# Install git-code
+# Install (or update) git-code
 bash <(curl -fsSL https://leogr.github.io/git-code/install.sh)
